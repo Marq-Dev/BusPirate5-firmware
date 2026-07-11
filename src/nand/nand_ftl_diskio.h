15be3e6f488ef30b13ee69e67083d229bf77ac9c
Marq-Dev <9102154+Marq-Dev@users.noreply.github.com>
2026-07-11 11:29:57 +0000

    feat: add NAND deep-format storage recovery

--- src/nand/nand_ftl_diskio.h
+++ src/nand/nand_ftl_diskio.h
@@ -10,8 +10,26 @@
 #ifndef __NAND_FTL_DISKIO_H
 #define __NAND_FTL_DISKIO_H
 
+#include <stdbool.h>
+
 // #include "../fatfs/diskio.h" // types from the diskio driver
 // #include "../fatfs/ff.h"     // BYTE type
+
+typedef enum {
+    DISKIO_DEEP_RESET_STAGE_NONE = 0,
+    DISKIO_DEEP_RESET_STAGE_INVALID_DRIVE,
+    DISKIO_DEEP_RESET_STAGE_NAND_INIT,
+    DISKIO_DEEP_RESET_STAGE_NAND_ERASE,
+    DISKIO_DEEP_RESET_STAGE_FTL_INIT,
+} diskio_deep_reset_stage_t;
+
+typedef struct {
+    diskio_deep_reset_stage_t stage;
+    int status;
+} diskio_deep_reset_result_t;
+
+bool diskio_deep_reset(BYTE drv,
+                       diskio_deep_reset_result_t *result);
 
 DSTATUS diskio_initialize(BYTE drv);
 DSTATUS diskio_status(BYTE drv);
